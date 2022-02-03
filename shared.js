0b5d4e94b65ca0cfa6d245fca6ff812fbc4766c3
Tom Robinson <tomrobin@cisco.com>
2022-02-03 16:26:37 +0000

    Move function

--- shared.js
+++ shared.js
@@ -1,5 +1,26 @@
  var app = new window.Webex.Application();
 
+ function handleShareStateChange(event) {
+    let appSharing = event.isSharing
+    const newDiv = document.createElement("div");
+
+    // and give it some content
+    let message = "";
+    if (appSharing) {
+        message = "Hello Everyone I am Sharing"
+    } else {
+        message = "Hello Everyone I am not Sharing :-("
+    }
+    const newContent = document.createTextNode(message);
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+ }
+
  // Wait for onReady promise, handle error scenario
  app.onReady().then(() => {
      log("Application ready. App", app);
@@ -31,25 +52,4 @@
      let payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
      li.appendChild(payload)
      ul.prepend(li);
- }
-
- function handleShareStateChange(event) {
-    let appSharing = event.isSharing
-    const newDiv = document.createElement("div");
-
-    // and give it some content
-    let message = "";
-    if (appSharing) {
-        message = "Hello Everyone I am Sharing"
-    } else {
-        message = "Hello Everyone I am not Sharing :-("
-    }
-    const newContent = document.createTextNode(message);
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
  }
